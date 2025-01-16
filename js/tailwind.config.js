154274182df0d40f6c3afc5bd2707e767fd625fa
Humayun Kabir <humayunkabir756@gmail.com>
2025-01-16 23:10:24 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // colors
+        colors: {
+          btnColor: '#8454F5',
+          formColor: '#F3EEFE',
+          sH1Color: '#ffc107',
+          sH2Color: '#6610f2',
+          sH3Color: '#20c997',
+          sH4Color: '#fd7e14',
+          sH5Color: '#0dcaf0',
+          sH6Color: '#d63384',
+          tabBgColor: '#F5F5FF',
+          priceColor: '#181059',
+          priceBtnColor: '#F3EEFE',
+        },
+        // colors
+        // container
+        maxWidth: {
+            'allContainer': '1280px',
+          },
+        // container
+        // background Image
+        backgroundImage: {
+            'headerBg': "url('./images/headerBannerBg.jpg')",
+            'businessTask': "url('./images/businessTbg.jpg')",
+            'artificialBg': "url('./images/ArtificialBg.jpg')",
+          },
+        // background Image
+        // fontsfamily
+        fontFamily: {
+            'open': ["Open Sans"],
+            'nuni': ["Nunito"],
+          },
+        // fontsfamily
+      }
+    }
+  }
